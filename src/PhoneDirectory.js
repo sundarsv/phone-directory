a7e1f62a8d9faabb7346cf695b5834ed417762e2
Srinivasan <sundarsv@gmail.com>
2018-10-07 14:25:18 +0530

    Added Delete Subscriber

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -21,7 +21,21 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
             ]
         }
     }
-     addSubscriberHandler = (newSubscriber) => {
+
+    deleteSubscriberHandler = (subscriberId) => {
+        let subscribersList = this.state.subscribersList;
+        let subscriberIndex = 0;
+        subscribersList.forEach (function (subscriber, index) {
+            if (subscriber.id === subscriberId ) {
+                subscriberIndex = index
+            }
+        }, this);
+        let newSubscribers = subscribersList;
+        newSubscribers.splice(subscriberIndex, 1);
+        this.setState({subscribers : newSubscribers});
+    }
+
+    addSubscriberHandler = (newSubscriber) => {
         let subscribersList = this.state.subscribersList;
         if (subscribersList.length > 0) {
             newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
@@ -31,11 +45,11 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
         subscribersList.push(newSubscriber);
         this.setState({ subscribersList: subscribersList });
     }
-     render() {
+    render() {
         return (
                 <Router>
                     <div>
-                    <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList = {this.state.subscribersList} />} />
+                    <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList = {this.state.subscribersList} deleteSubscriberHandler={this.deleteSubscriberHandler} />} />
                     <Route exact path="/add" render={({history}, props) => <AddSubscriber history = {history} {...props} addSubscriberHandler = {this.addSubscriberHandler} />} />
                     </div>
                 </Router>
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -5,6 +5,10 @@ import {Link} from 'react-router-dom';
 
 class ShowSubscribers extends Component {
 
+  onDeleteClick = (subscriberId) => {
+    this.props.deleteSubscriberHandler(subscriberId);
+  }
+  
   render() {
     return (
       <div>
@@ -23,7 +27,7 @@ class ShowSubscribers extends Component {
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
                 <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
+                  <button className="custom-btn delete-btn" onClick={this.onDeleteClick.bind(this, sub.id)}>Delete</button>
                 </span>
               </div>
             })
